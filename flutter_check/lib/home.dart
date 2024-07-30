02800239af82b9bb9ef36178f4d0c03ba2c7ec71
FrancisCarolina <francis.carolinalovo@gmail.com>
2024-07-30 10:31:50 -0300

    teste user global

--- flutter_check/lib/home.dart
+++ flutter_check/lib/home.dart
@@ -11,7 +11,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool cbIsSelected_1 = false;
-
+//teste
   @override
   Widget build(BuildContext context) {
     return Scaffold(
